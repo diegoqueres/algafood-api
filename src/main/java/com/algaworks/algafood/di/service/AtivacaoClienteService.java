49b509ce62d4e061ae71da9a79e3ebe73a755db0
Diego Queres <diegoqueres81@gmail.com>
2022-04-22 19:13:08 -0300

    2.13. Usando @Configuration e @Bean para definir beans

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -2,9 +2,7 @@ package com.algaworks.algafood.di.service;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.Notificador;
-import org.springframework.stereotype.Component;
 
-@Component
 public class AtivacaoClienteService {
 
     private final Notificador notificador;
@@ -16,8 +14,6 @@ public class AtivacaoClienteService {
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
-
         this.notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
     }
-
 }
