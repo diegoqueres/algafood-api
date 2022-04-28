56a6893cbb1dd986752b200bfcc234b106967eb2
Diego Queres <diegoqueres81@gmail.com>
2022-04-28 19:20:14 -0300

    2.22. Publicando e consumindo eventos customizados

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,35 +1,20 @@
 package com.algaworks.algafood.di.service;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import com.algaworks.algafood.di.notificacao.NiverUrgencia;
-import com.algaworks.algafood.di.notificacao.Notificador;
-import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
-//@Component
+@Component
 public class AtivacaoClienteService {
 
-    @TipoDoNotificador(NiverUrgencia.SEM_URGENCIA)
     @Autowired
-    private Notificador notificador;
-
-//    @PostConstruct
-    public void init() {
-        System.out.println("INIT " + notificador);
-    }
-
-//    @PreDestroy
-    public void destroy() {
-        System.out.println("DESTROY");
-    }
+    private ApplicationEventPublisher eventPublisher;
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
-        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+        // dizer para o container que o cliente está sendo ativado neste momento
+        eventPublisher.publishEvent(new ClienteAtivadoEvent(cliente));
     }
 }
