19840c6907ce7464aebfeb612f71b095b8248f08
Diego Queres <diegoqueres81@gmail.com>
2022-05-03 20:18:33 -0300

    2.25. Criando e acessando propriedades customizadas com @Value

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.di.notificacao;
 
 import com.algaworks.algafood.di.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -8,13 +9,17 @@ import org.springframework.stereotype.Component;
 @TipoDoNotificador(NiverUrgencia.SEM_URGENCIA)
 @Component
 public class NotificadorEmailMock implements Notificador {
+    @Value("${notificador.email.host-servidor}")
+    private String host;
 
-    public NotificadorEmailMock() {
-        System.out.println("NotificadorEmail MOCK");
-    }
+    @Value("${notificador.email.porta-servidor}")
+    private Integer porta;
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
+        System.out.println("Host: " + host);
+        System.out.println("Porta: " + porta);
+
         System.out.printf("MOCK: Notificação seria enviada para %s através do e-mail %s: %s\n",
                 cliente.getNome(), cliente.getEmail(), mensagem);
     }
