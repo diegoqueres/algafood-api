1170488af263f425c443df5213df22def8f499bd
Diego Queres <diegoqueres81@gmail.com>
2022-06-14 17:57:07 -0300

    4.11. Desafio: collection resource de estados

--- src/main/java/com/algaworks/algafood/domain/repository/EstadoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/EstadoRepository.java
@@ -5,7 +5,7 @@ import com.algaworks.algafood.domain.model.Estado;
 import java.util.List;
 
 public interface EstadoRepository {
-    List<Estado> todas();
+    List<Estado> listar();
     Estado porId(Long id);
     Estado adicionar(Estado estado);
     void remover(Estado estado);
