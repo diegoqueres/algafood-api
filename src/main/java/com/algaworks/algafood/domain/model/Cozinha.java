4b0102ccbf9d863b327a9bf5c931e0e4c1c7b787
Diego Queres <diegoqueres81@gmail.com>
2022-06-17 19:18:19 -0300

    4.16. Customizando a representação em XML com Wrapper e anotações do Jackson

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,14 +1,12 @@
 package com.algaworks.algafood.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
-@JsonRootName("gastronomia")
+@JsonRootName("cozinha")
 @Entity
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -19,8 +17,6 @@ public class Cozinha {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //  @JsonIgnore
-    @JsonProperty("titulo")
     @Column(nullable = false)
     private String nome;
 }
