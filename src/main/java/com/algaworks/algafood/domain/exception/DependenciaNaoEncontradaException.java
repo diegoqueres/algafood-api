d315388f7e6dfa7143ec62ff7f7d8c708880ee64
Diego Queres <diegoqueres81@gmail.com>
2022-06-23 10:27:52 -0300

    5.4. Refatorando o código do projeto para usar o repositório do SDJ

--- src/main/java/com/algaworks/algafood/domain/exception/DependenciaNaoEncontradaException.java
+++
@@ -1,9 +0,0 @@
-package com.algaworks.algafood.domain.exception;
-
-public class DependenciaNaoEncontradaException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public DependenciaNaoEncontradaException(String mensagem) {
-        super(mensagem);
-    }
-}
