f38c06438e2d0be365c8be2929de56eab2482f13
Diego Queres <diegoqueres81@gmail.com>
2022-06-26 16:36:10 -0300

    8.18. Padronizando o formato de problemas no corpo de respostas com a RFC 7807

---
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -0,0 +1,18 @@
+package com.algaworks.algafood.api.exceptionhandler;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+@Builder
+public class Problem {
+
+    // Adjustment to RFC 7807 (Problem Details for HTTP APIs)
+    private Integer status;
+    private String type;
+    private String title;
+    private String detail;
+
+}
