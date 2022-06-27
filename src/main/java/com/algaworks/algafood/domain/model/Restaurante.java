d0ef828848054aa57fd6911233dd2f98dc94fc84
Diego Queres <diegoqueres81@gmail.com>
2022-06-27 16:31:28 -0300

    9.13. Resolvendo mensagens de validação com Resource Bundle do Bean Validation

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -45,7 +45,7 @@ public class Restaurante {
 	private String nome;
 
 	@NotNull
-	@PositiveOrZero
+	@PositiveOrZero(message = "{TaxaFrete.invalida}")
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 
