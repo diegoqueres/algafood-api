a931a0e5ed68e92d80f20e6ca718016793c52e55
Diego Queres <diegoqueres81@gmail.com>
2022-07-08 18:53:41 -0300

    10.12. Voltando o estado inicial do banco de dados para cada execução de teste com callback do Flyway

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -2,9 +2,11 @@ package com.algaworks.algafood;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.flywaydb.core.Flyway;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -21,11 +23,16 @@ public class CadastroCozinhaIT {
 	@LocalServerPort
 	private int port;
 
+	@Autowired
+	private Flyway flyway;
+
 	@Before
 	public void setUp() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
+
+		flyway.migrate();
 	}
 
 	@Test
@@ -50,8 +57,7 @@ public class CadastroCozinhaIT {
 	}
 
 	@Test
-	//public void deveRetornarStatus201_QuandoCadastrarCozinha() {
-	public void testRetornarStatus201_QuandoCadastrarCozinha() {
+	public void deveRetornarStatus201_QuandoCadastrarCozinha() {
 		given()
 				.body("{ \"nome\": \"Chinesa\" }")
 				.accept(ContentType.JSON)
