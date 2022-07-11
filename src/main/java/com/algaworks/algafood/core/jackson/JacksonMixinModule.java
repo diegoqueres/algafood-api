20a20f7d3688bc2ada42439207258420d576f68d
Diego Queres <diegoqueres81@gmail.com>
2022-07-11 20:40:26 -0300

    11.4. Desafio: usando @JsonIgnoreProperties e Jackson Mixin

--- src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
@@ -1,6 +1,10 @@
 package com.algaworks.algafood.core.jackson;
 
+import com.algaworks.algafood.api.model.CidadeMixin;
+import com.algaworks.algafood.api.model.CozinhaMixin;
 import com.algaworks.algafood.api.model.RestauranteMixin;
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.springframework.stereotype.Component;
@@ -11,6 +15,8 @@ public class JacksonMixinModule extends SimpleModule {
 
     public JacksonMixinModule() {
         setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
+        setMixInAnnotation(Cidade.class, CidadeMixin.class);
+        setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
     }
 
 }
--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,22 +1,14 @@
 package com.algaworks.algafood.domain.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
 import com.algaworks.algafood.core.validation.Groups;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -32,8 +24,7 @@ public class Cozinha {
 	@NotBlank
 	@Column(nullable = false)
 	private String nome;
-	
-	@JsonIgnore
+
 	@OneToMany(mappedBy = "cozinha")
 	private List<Restaurante> restaurantes = new ArrayList<>();
 	
