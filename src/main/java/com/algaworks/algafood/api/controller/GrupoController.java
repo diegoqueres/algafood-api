07460cbb469e8b26b19c32042701c319f3df64cd
Diego Queres <diegoqueres81@gmail.com>
2022-07-29 19:28:40 -0300

    12.8. Desafio: implementando os endpoints de grupos

---
+++ src/main/java/com/algaworks/algafood/api/controller/GrupoController.java
@@ -0,0 +1,67 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.api.assembler.GrupoInputDisassembler;
+import com.algaworks.algafood.api.assembler.GrupoModelAssembler;
+import com.algaworks.algafood.api.model.GrupoModel;
+import com.algaworks.algafood.api.model.input.GrupoInput;
+import com.algaworks.algafood.domain.model.Grupo;
+import com.algaworks.algafood.domain.repository.GrupoRepository;
+import com.algaworks.algafood.domain.service.CadastroGrupoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/grupos")
+public class GrupoController {
+
+    @Autowired
+    private CadastroGrupoService cadastroGrupo;
+
+    @Autowired
+    private GrupoRepository grupoRepository;
+
+    @Autowired
+    private GrupoInputDisassembler grupoInputDisassembler;
+
+    @Autowired
+    private GrupoModelAssembler grupoModelAssembler;
+
+    @GetMapping
+    public List<GrupoModel> listar() {
+        List<Grupo> todosGrupos = grupoRepository.findAll();
+
+        return grupoModelAssembler.toCollectionModel(todosGrupos);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public GrupoModel adicionar(@Valid @RequestBody GrupoInput grupoInput) {
+        Grupo grupo = grupoInputDisassembler.toDomainObject(grupoInput);
+
+        grupo = cadastroGrupo.salvar(grupo);
+
+        return grupoModelAssembler.toModel(grupo);
+    }
+
+    @PutMapping("/{grupoId}")
+    public GrupoModel atualizar(@PathVariable Long grupoId, @Valid @RequestBody GrupoInput grupoInput) {
+        Grupo grupoAtual = cadastroGrupo.buscarOuFalhar(grupoId);
+
+        grupoInputDisassembler.copyToDomainObject(grupoInput, grupoAtual);
+
+        grupoAtual = cadastroGrupo.salvar(grupoAtual);
+
+        return grupoModelAssembler.toModel(grupoAtual);
+    }
+
+    @DeleteMapping("/{grupoId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void apagar(@PathVariable Long grupoId) {
+        cadastroGrupo.excluir(grupoId);
+    }
+
+}
