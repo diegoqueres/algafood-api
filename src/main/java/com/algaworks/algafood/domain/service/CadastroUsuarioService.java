2f0aa162aba82ee4aab2785ee6c04867e9afe2fc
Diego Queres <diegoqueres81@gmail.com>
2022-07-29 20:25:16 -0300

    12.9. Desafio: implementando os endpoints de usuarios

---
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
@@ -0,0 +1,44 @@
+package com.algaworks.algafood.domain.service;
+
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.exception.NegocioException;
+import com.algaworks.algafood.domain.exception.UsuarioNaoEncontradoException;
+import com.algaworks.algafood.domain.model.Usuario;
+import com.algaworks.algafood.domain.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CadastroUsuarioService {
+
+    private static final String MSG_FORMA_USUARIO_EM_USO
+            = "Usuário de código %d não pode ser removido, pois está em uso";
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Transactional
+    public void alterarSenha(Long usuarioId, String senhaAtual, String novaSenha) {
+        Usuario usuario = buscarOuFalhar(usuarioId);
+
+        if (usuario.senhaNaoCoincideCom(senhaAtual)) {
+            throw new NegocioException("Senha atual informada não coincide com a senha do usuário.");
+        }
+
+        usuario.setSenha(novaSenha);
+    }
+
+    public Usuario buscarOuFalhar(Long usuarioId) {
+        return usuarioRepository.findById(usuarioId)
+                .orElseThrow(() -> new UsuarioNaoEncontradoException(usuarioId));
+    }
+
+}
