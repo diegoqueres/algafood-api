00702b6a765ae73291ccc94f590c658cc20d0a4e
Diego Queres <diegoqueres81@gmail.com>
2022-10-13 17:12:11 -0300

    14.22. Definindo bean do client da Amazon S3 e configurando credenciais

--- src/main/java/com/algaworks/algafood/infrastructure/service/storage/LocalFotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/LocalFotoStorageService.java
@@ -3,7 +3,6 @@ package com.algaworks.algafood.infrastructure.service.storage;
 import com.algaworks.algafood.core.storage.StorageProperties;
 import com.algaworks.algafood.domain.service.FotoStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 
--- src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
@@ -1,10 +1,15 @@
 package com.algaworks.algafood.infrastructure.service.storage;
 
 import com.algaworks.algafood.domain.service.FotoStorageService;
+import com.amazonaws.services.s3.AmazonS3;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.InputStream;
 
 public class S3FotoStorageService implements FotoStorageService {
+
+    @Autowired
+    private AmazonS3 amazonS3;
 
     @Override
     public InputStream recuperar(String nomeArquivo) {
@@ -20,5 +25,5 @@ public class S3FotoStorageService implements FotoStorageService {
     public void remover(String nomeArquivo) {
 
     }
-    
+
 }
