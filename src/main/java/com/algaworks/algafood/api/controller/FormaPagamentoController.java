bce584fc24117d6c145f2f78bd42e02c22d294ce
Diego Queres <diegoqueres81@gmail.com>
2022-10-24 09:50:04 -0300

    17.6. Adicionando outras diretivas de Cache-Control na resposta HTTP

--- src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
@@ -52,7 +52,11 @@ public class FormaPagamentoController {
 				.toCollectionModel(todasFormasPagamentos);
 		
 		return ResponseEntity.ok()
-				.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+//				.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+//				.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePrivate())
+				.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePublic())
+//				.cacheControl(CacheControl.noCache())
+//				.cacheControl(CacheControl.noStore())
 				.body(formasPagamentosModel);
 	}
 	
