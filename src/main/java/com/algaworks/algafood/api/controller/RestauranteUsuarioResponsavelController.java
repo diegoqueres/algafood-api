8c0c2f33e8e63283cd7d1b17fc202699ab4d1aa8
Diego Queres <diegoqueres81@gmail.com>
2023-01-02 17:52:15 -0300

    19.12. Desafio: adicionando hypermedia nos recursos de usuários

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteUsuarioResponsavelController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteUsuarioResponsavelController.java
@@ -6,6 +6,7 @@ import com.algaworks.algafood.api.openapi.controller.RestauranteUsuarioResponsav
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,8 +16,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "/restaurantes/{restauranteId}/responsaveis")
@@ -30,7 +29,7 @@ public class RestauranteUsuarioResponsavelController implements RestauranteUsuar
 
 	@Override
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<UsuarioModel> listar(@PathVariable Long restauranteId) {
+	public CollectionModel<UsuarioModel> listar(@PathVariable Long restauranteId) {
 		Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
 		
 		return usuarioModelAssembler.toCollectionModel(restaurante.getResponsaveis());
