0d98bb0b4ac3d57b85e08934b17cd3bca47d8206
Diego Queres <diegoqueres81@gmail.com>
2023-01-19 19:43:46 -0300

    19.29. Adicionando links com template variable de caminho de formas de pagamento do restaurante

--- src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteFormaPagamentoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteFormaPagamentoControllerOpenApi.java
@@ -1,14 +1,16 @@
 package com.algaworks.algafood.api.openapi.controller;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.ResponseEntity;
+
 import com.algaworks.algafood.api.exceptionhandler.Problem;
 import com.algaworks.algafood.api.model.FormaPagamentoModel;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.http.ResponseEntity;
 
 @Api(tags = "Restaurantes")
 public interface RestauranteFormaPagamentoControllerOpenApi {
@@ -40,7 +42,7 @@ public interface RestauranteFormaPagamentoControllerOpenApi {
 			@ApiResponse(code = 404, message = "Restaurante ou forma de pagamento não encontrado",
 					response = Problem.class)
 	})
-	void associar(
+	ResponseEntity<Void> associar(
 			@ApiParam(value = "ID do restaurante", example = "1", required = true)
 					Long restauranteId,
 
