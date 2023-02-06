3bc783936b3addc840c92efb19d602eebc68ad5d
Diego Queres <diegoqueres81@gmail.com>
2023-02-06 19:36:44 -0300

    20.11. Implementando o versionamento da API por Media Type

--- src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -1,13 +1,14 @@
 package com.algaworks.algafood.core.modelmapper;
 
+import com.algaworks.algafood.api.v1.model.EnderecoModel;
+import com.algaworks.algafood.api.v1.model.input.ItemPedidoInput;
+import com.algaworks.algafood.api.v2.model.input.CidadeInputV2;
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.model.Endereco;
+import com.algaworks.algafood.domain.model.ItemPedido;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.algaworks.algafood.api.v1.model.EnderecoModel;
-import com.algaworks.algafood.api.v1.model.input.ItemPedidoInput;
-import com.algaworks.algafood.domain.model.Endereco;
-import com.algaworks.algafood.domain.model.ItemPedido;
 
 @Configuration
 public class ModelMapperConfig {
@@ -15,7 +16,10 @@ public class ModelMapperConfig {
 	@Bean
 	public ModelMapper modelMapper() {
 		var modelMapper = new ModelMapper();
-		
+
+		modelMapper.createTypeMap(CidadeInputV2.class, Cidade.class)
+				.addMappings(mapper -> mapper.skip(Cidade::setId));
+
 //		modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
 //			.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
 		
